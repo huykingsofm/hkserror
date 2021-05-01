82a197216f3f7b8685d6f4ea43450be8e0c314f5
Le Huy <huykingsofm@gmail.com>
2021-05-02 00:53:33 +0700

    Add some common error

---
+++ tests/test_hkserror.py
@@ -0,0 +1,21 @@
+import traceback
+
+from hkserror import HTypeError, HKSError
+
+def test_htypeerror():
+    a = 5
+    try:
+        raise HTypeError("a", a, float, None)
+    except HKSError as e:
+        print(e)
+
+
+def test_hkserror():
+    try:
+        raise HKSError("huy")
+    except Exception as e:
+        print(e)
+
+
+if __name__ == "__main__":
+    test_hkserror()
