e7e795958f95c080dfc3490b25db70f11e67fed7
Le Huy <huykingsofm@gmail.com>
2021-05-10 11:09:25 +0700

    Add changelog to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,11 @@ import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+    long_description += "\n"
+
+
+with open("CHANGELOG.md", "r", encoding="utf-8") as fh:
+    long_description += fh.read()
 
 
 setuptools.setup(
